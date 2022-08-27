33922327bdd7b737bfb671729cbe4f5e51173feb
logica <84759675+logica0419@users.noreply.github.com>
2022-08-27 10:43:00 +0900

    cutout login

--- sql/init.sh
+++ sql/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-cd `dirname $0`
+cd "$(dirname "$0")"
 
 ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
 ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
@@ -9,30 +9,30 @@ ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isucon}
 
 mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < 3_schema_exclude_user_presents.sql
+  -p"$ISUCON_DB_PASSWORD" \
+  --host "$ISUCON_DB_HOST" \
+  --port "$ISUCON_DB_PORT" \
+  "$ISUCON_DB_NAME" <3_schema_exclude_user_presents.sql
 
 mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < 4_alldata_exclude_user_presents.sql
+  -p"$ISUCON_DB_PASSWORD" \
+  --host "$ISUCON_DB_HOST" \
+  --port "$ISUCON_DB_PORT" \
+  "$ISUCON_DB_NAME" <4_alldata_exclude_user_presents.sql
 
 echo "delete from user_presents where id > 100000000000" | mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME"
+  -p"$ISUCON_DB_PASSWORD" \
+  --host "$ISUCON_DB_HOST" \
+  --port "$ISUCON_DB_PORT" \
+  "$ISUCON_DB_NAME"
 
-DIR=`mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" -h "$ISUCON_DB_HOST" -Ns -e "show variables like 'secure_file_priv'" | cut -f2`
+DIR=$(mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" -h "$ISUCON_DB_HOST" -Ns -e "show variables like 'secure_file_priv'" | cut -f2)
 SECURE_DIR=${DIR:-/var/lib/mysql-files/}
 
-sudo cp 5_user_presents_not_receive_data.tsv ${SECURE_DIR}
+sudo cp 5_user_presents_not_receive_data.tsv "${SECURE_DIR}"
 
 echo "LOAD DATA INFILE '${SECURE_DIR}5_user_presents_not_receive_data.tsv' REPLACE INTO TABLE user_presents FIELDS ESCAPED BY '|' IGNORE 1 LINES ;" | mysql -u"$ISUCON_DB_USER" \
-        -p"$ISUCON_DB_PASSWORD" \
-        --host "$ISUCON_DB_HOST" \
-        --port "$ISUCON_DB_PORT" \
-        "$ISUCON_DB_NAME" 
+  -p"$ISUCON_DB_PASSWORD" \
+  --host "$ISUCON_DB_HOST" \
+  --port "$ISUCON_DB_PORT" \
+  "$ISUCON_DB_NAME"
