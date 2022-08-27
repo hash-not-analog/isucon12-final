d8976e85d3df7681d2ad65ea9d63faba17488da0
logica <84759675+logica0419@users.noreply.github.com>
2022-08-27 17:55:16 +0900

    Revert "fix"
    This reverts commit bac399cf8f8437ec22c7c4ae7b087899ce8e8d33.

--- go/present.go
+++ go/present.go
@@ -164,7 +164,7 @@ func (h *Handler) receivePresent(c echo.Context) error {
 	if err != nil {
 		return errorResponse(c, http.StatusInternalServerError, err)
 	}
-	defer tx2.Rollback() //nolint:errcheck
+	defer tx.Rollback() //nolint:errcheck
 
 	query = "INSERT INTO user_presents(id, user_id, sent_at, item_type, item_id, amount, present_message, created_at, deleted_at, updated_at)" +
 		" VALUES (:id, :user_id, :sent_at, :item_type, :item_id, :amount, :present_message, :created_at, :deleted_at, :updated_at)" +
